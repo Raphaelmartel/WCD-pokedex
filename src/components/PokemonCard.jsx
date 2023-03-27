9bee06c82dd1a1027a33dbcb9f51dec7409fb29b
Raphaël Martel <contact@raphaelmartel.com>
2023-03-27 15:22:17 +0200

    Add const pokemonList

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,19 @@
+const pokemonList = [
+  {
+    name: 'bulbasaur',
+    imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+  },
+  {
+    name: 'mew',
+  },
+];
+
 export function PokemonCard() {
+  const pokemon = pokemonList[0];
+
   return (
     <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" />
+      <img src="" alt="bulbasaur" />
       <figcaption>bulbasaur</figcaption>
     </figure>
   );
