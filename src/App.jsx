890941b06dd09a01be09b6727474edc89d756086
Raphaël Martel <contact@raphaelmartel.com>
2023-03-28 10:03:21 +0200

    Passing props

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,32 @@ import { useState } from 'react';
 import './App.css';
 import { PokemonCard } from './components/PokemonCard';
 
+const pokemonList = [
+  {
+    name: 'Bulbasaur',
+    imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+  },
+  {
+    name: 'Mew',
+  },
+  {
+    name: 'Charmander',
+    imgSrc: 'https://www.pngmart.com/files/13/Pokemon-Charmander-PNG-Pic.png',
+  },
+  {
+    name: 'Squirtle',
+    imgSrc: 'https://www.waouo.com/wp-content/uploads/2011/09/carapucepokemon.png',
+  },
+  {
+    name: 'Pikachu',
+    imgSrc: 'https://www.pngall.com/wp-content/uploads/5/Pokemon-Pikachu-PNG.png',
+  },
+];
+
 function App() {
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[4]} />
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,29 +1,17 @@
-const pokemonList = [
-  {
-    name: 'bulbasaur',
-    imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
-  },
-  {
-    name: 'mew',
-    imgSrc: '',
-  },
-];
-
-export function PokemonCard() {
-  const pokemon = pokemonList[1];
-
-  const pokemonDisplay = (pokemon) => {
-    if (pokemon.imgSrc === undefined || pokemon.imgSrc === '') {
-      return <p>???</p>;
-    } else {
-      return <img src={pokemon.imgSrc} alt="" />;
-    }
-  };
-
+export function PokemonCard({ pokemon }) {
+  
   return (
     <figure>
       {pokemonDisplay(pokemon)}
       <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
+}
+
+function pokemonDisplay(pokemon) {
+  if (pokemon.imgSrc === undefined || pokemon.imgSrc === '') {
+    return <p>???</p>;
+  } else {
+    return <img src={pokemon.imgSrc} alt="" />;
+  }
 }
